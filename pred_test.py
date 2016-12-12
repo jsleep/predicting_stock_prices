8dec84bc8cb51f2f0deee91a51eb02d087788a1b
Ryan Smith <ryans_cais@yahoo.com>
2016-12-12 01:06:33 -0800

    Added option for regressing against price differences if we want

--- pred_test.py
+++ pred_test.py
@@ -4,6 +4,7 @@ from sklearn.svm import SVR
 from sklearn.neural_network import MLPRegressor
 import matplotlib.pyplot as plt
 from dateutil.parser import parse
+from datetime import datetime, timedelta
 
 comp_names = [
     'Amazon', 'Facebook', 'IBM', 'Oracle', 'Twitter',
@@ -27,13 +28,36 @@ svr_params = {
 }
 
 nnr_params = {
-    'hidden_layer_sizes': (22, 11),
+    'hidden_layer_sizes': (8, 5),
     'solver': 'lbfgs',
+    'activation': 'logistic',
     'max_iter': 1000
 }
 
-dates = []
-prices = []
+
+
+def get_num_for_date(date):
+    return (date - parse('Dec 5 2011')).days
+
+def get_date_for_num(num):
+    new_date = parse('Dec 5 2011') + timedelta(days=num)
+    return new_date
+
+def add_bus_days(datenum, num, comp):
+    num_its = 0
+    next_day = datenum + num
+    while next_day not in data[comp].keys() and num_its < 10:
+        next_day += 1
+        num_its += 1
+    return next_day
+
+def minus_bus_days(datenum, num, comp):
+    num_its = 0
+    next_day = datenum - num
+    while next_day not in data[comp].keys() and num_its < 10:
+        next_day -= 1
+        num_its += 1
+    return next_day
 
 def get_all_data():
     for comp_name in comp_names:
@@ -42,49 +66,89 @@ def get_all_data():
             next(csvfileReader)
             for row in csvfileReader:
                 if row[1] != '-':
-                    data[comp_name][parse(row[0])] = float(row[1])
+                    data[comp_name][get_num_for_date(parse(row[0]))] = float(row[1])
 
-def get_features(comp, date):
-    return [data[comp][date]]
+DAYS_BACK = 10
 
-def run_cross_fold_validation(comp, k_fold=10, data_subset=None):
-    if data_subset:
-        pass
+def get_features(comp, date_num):
+    features = []
+    features.append(date_num)
+    for ndx in range(1, 11):
+        if minus_bus_days(date_num, ndx, comp) in data.keys():
+            features.append(data[comp][minus_bus_days(date_num, ndx, comp)])
+        else:
+            features.append(0)
+    features.append(get_date_for_num(date_num).month)
+    features.append(get_date_for_num(date_num).day + 30 * get_date_for_num(date_num).month)
+
+    return features
+
+def run_cross_fold_validation(comp, k_fold=10, data_subset=None, diff=False):
+    total = []
+
+    if data_subset is None:
+        data_subset = data[comp].keys()
+
+    for date_num in data_subset:
+        features = get_features(comp, date_num)
+        if features:
+            if diff:
+                total.append((features, (data[comp][date_num]) - (data[comp][minus_bus_days(date_num, 1, comp)])))
+            else:
+                total.append((features, data[comp][date_num]))
+    random.shuffle(total)
+
+    svr_avg_acc = 0
+    nnr_avg_r2 = 0
+
+    for ndx in range(k_fold):
+        startndx = int(ndx / k_fold * len(total))
+        endndx = int((ndx + 1) / k_fold * len(total))
+        test = total[startndx:endndx]
+        train = total[:startndx]
+        train.extend(total[endndx:])
+
+        # Neural Net, because why not
+        nnr = MLPRegressor(**nnr_params)
+        nnr.fit([x[0] for x in train], [x[1] for x in train])
+
+        # Switch this to whatever you want, like below
+        #svr_lin = SVR(**svr_params)
+        #svr_lin.fit([x[0] for x in train], [x[1] for x in train])
+
+        #svr_acc = svr_lin.score([x[0] for x in test], [x[1] for x in test])
+        nnr_acc = nnr.score([x[0] for x in test], [x[1] for x in test])
+
+        #svr_avg_acc += svr_acc
+        nnr_avg_r2 += nnr_acc
+
+        print(nnr_acc, sep=", ")
+
+    #svr_avg_acc /= k_fold
+    nnr_avg_r2 /= k_fold
+
+    #print('Avg SVM R^2 = ' + str(svr_avg_acc))
+    print('Avg Neural Net R^2 = ' + str(nnr_avg_r2))
+
+    datenums = data_subset
+    if diff:
+        prices = [(data[comp][datenum] - data[comp][minus_bus_days(datenum, 1, comp)]) for datenum in datenums]
     else:
-        total = []
+        prices = [data[comp][datenum] for datenum in datenums]
 
-        for date in data[comp].keys():
-            features = get_features(comp, date)
-            if features:
-                total.append((features, data[comp][date]))
-        random.shuffle(total)
+    plt.scatter(datenums, prices, color='black', label='Data', marker='o')  # plotting the initial datapoints
+    if diff:
+        plt.plot(datenums, nnr.predict([get_features(comp, datenum) for datenum in datenums]),
+                 color='red',label='NNR model')  # plotting the line made by the RBF kernel
+    else:
+        plt.plot(datenums, nnr.predict([get_features(comp, datenum) for datenum in datenums]),
+                 color='red', label='NNR model')  # plotting the line made by the RBF kernel
+    plt.xlabel('Date')
+    plt.ylabel('Price')
+    plt.title('Support Vector Regression')
+    plt.legend()
+    plt.show()
 
-        svr_avg_acc = 0
-        nnr_avg_r2 = 0
-
-        for ndx in range(k_fold):
-            startndx = int(ndx / k_fold * len(total))
-            endndx = int((ndx + 1) / k_fold * len(total))
-            test = total[startndx:endndx]
-            train = total[:startndx]
-            train.extend(total[endndx:])
-
-            # Neural Net, because why not
-            nnr = MLPRegressor(**nnr_params)
-            nnr.fit([x[0] for x in train], [x[1] for x in train])
-
-            # Switch this to whatever you want, like below
-            svr_lin = SVR(**svr_params)
-            svr_lin.fit([x[0] for x in train], [x[1] for x in train])
-
-            svr_avg_acc += svr_lin.score([x[0] for x in test], [x[1] for x in test])
-            nnr_avg_r2 += nnr.score([x[0] for x in test], [x[1] for x in test])
-
-        svr_avg_acc /= k_fold
-        nnr_avg_r2 /= k_fold
-
-        print('Avg SVM R^2 = ' + str(svr_avg_acc))
-        print('Avg Neural Net R^2 = ' + str(nnr_avg_r2))
 
 
 def predict_price(dates, prices, x):
@@ -111,7 +175,7 @@ def predict_price(dates, prices, x):
 
 def main():
     get_all_data()
-    run_cross_fold_validation('Oracle')
+    run_cross_fold_validation('Oracle', data_subset=[datenum for datenum in data['Oracle'].keys() if datenum > DAYS_BACK], diff=False)
 
 
 if __name__ == '__main__':
